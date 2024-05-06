e394290c55741560c4e41358f3c0dfc2ada7ccfd
Nathan Newyen <nathan@nathannewyen.com>
2024-05-06 19:36:32 -0400

    update mars caculate

---
+++ mars_calc/target/debug/deps/mars_calc.d
@@ -0,0 +1,5 @@
+C:\Users\natha\code\rust\rust-all-projects\mars_calc\target\debug\deps\mars_calc.exe: src\main.rs
+
+C:\Users\natha\code\rust\rust-all-projects\mars_calc\target\debug\deps\mars_calc.d: src\main.rs
+
+src\main.rs:
---
+++ mars_calc/target/debug/mars_calc.d
@@ -0,0 +1,1 @@
+C:\Users\natha\code\rust\rust-all-projects\mars_calc\target\debug\mars_calc.exe: C:\Users\natha\code\rust\rust-all-projects\mars_calc\src\main.rs
